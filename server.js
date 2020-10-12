b18b242c7a2f6404fe3e059830c05e9643a4dd8c
Peyton Schlafley <pschlafley0@gmail.com>
2020-10-12 14:18:00 -0400

    add paramaters to search for an id of an animal, also added 404 error message if no reslut is found

--- server.js
+++ server.js
@@ -47,6 +47,11 @@ const filterByQuery = function(query, animalsArray) {
     return filteredResults;
 }; 
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -55,6 +60,16 @@ app.get('/api/animals', (req, res) => {
     console.log(req.query)
     res.json(results)
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
